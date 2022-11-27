df9c6bdd1f6fed7fb7ea720084f099025eb53626
Eduardo-bg01 <ebeltran8@uabc.edu.mx>
2022-11-26 20:39:51 -0800

    ++

--- app/config.py
+++ app/config.py
@@ -1,8 +1,5 @@
 class Config():
-<<<<<<< HEAD
+
     SECRET_KEY="abc1"
     SQLALCHEMY_DATABASE_URI="mysql://root:root@localhost/CRUDFlask"
-=======
-    SECRET_KEY = "abc1"
-    SQLALCHEMY_DATABASE_URI = ""
->>>>>>> af86ed3e86aa2656c59a8860b29965c80fc59832
+
