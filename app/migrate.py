0f24fb33fb5cee94f3ce753426ee9412215e8791
Eduardo-bg01 <ebeltran8@uabc.edu.mx>
2022-11-26 20:39:45 -0800

    Masomenos como si funciona

--- app/migrate.py
+++ app/migrate.py
@@ -1,5 +1,4 @@
 from app.models import *
-<<<<<<< HEAD
 def create_db():
     db.drop_all()
     db.create_al()
@@ -7,21 +6,6 @@ def create_db():
 def init_db():
     create_db()
     admin = Users(mail="Eduardo")
-=======
-
-
-def create_db():
-    db.drop_all()
-    db.create_all()
-
-
-def init_db():
-    create_db()
-    admin = Users(
-        name="Pedro"
-
-    )
->>>>>>> af86ed3e86aa2656c59a8860b29965c80fc59832
 
     db.session.add(admin)
     db.session.commit()
--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Users(db.Model):
-<<<<<<< HEAD
     __tablename__='user'
     id = db.Column(db.Integer, primary_key=True)
     mail = db.Column(db.String(20))
@@ -12,15 +11,3 @@ class Usuarios(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     mail = db.column(db.String(20))
     passwd = db.Column(db.String(20))
-=======
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-
-
-class Videojuegos(db.Model):
-    __tablename__ = 'videojuegos'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-    noplayers = db.Column(db.Integer)
->>>>>>> af86ed3e86aa2656c59a8860b29965c80fc59832
