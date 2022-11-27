42077d145a1b568eed25deb2eefc06d2af301862
Eduardo-bg01 <ebeltran8@uabc.edu.mx>
2022-11-26 18:46:12 -0800

    Igual no jala

---
+++ app/migrate.py
@@ -0,0 +1,17 @@
+from app.models import *
+
+
+def create_db():
+    db.drop_all()
+    db.create_all()
+
+
+def init_db():
+    create_db()
+    admin = Users(
+        mail="eduardo"
+
+    )
+
+    db.session.add(admin)
+    db.session.commit()
